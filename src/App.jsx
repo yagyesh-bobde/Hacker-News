8005dac1f01101ab6baba02e486892b307ef73ac
Yagyesh Bobde <yagyeshsrbobde@gmail.com>
2022-08-22 17:59:47 +0530

    added the route for :id news item

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import NavBar from './components/NavBar'
-import { Home, ContactUs } from './pages'
+import { Home, ContactUs, News } from './pages'
 import {
     Routes, 
     Route
@@ -19,6 +19,7 @@ const App = () => {
       <Routes>
         <Route exact path='/' element={<Home />} />
         <Route exact path='/contact' element={<ContactUs />} />
+        <Route exact path='/news/:id' element={<News />} />
       </Routes>
         </NewsState>
     <Footer />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,3 @@
 export { default as Home } from './Home'
 export { default as ContactUs } from './ContactUs'
+export { default as News } from './News'
